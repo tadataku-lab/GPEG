7abf16d0ac15c6af962fa8b3c868c2c9ca5266f4
taku tada <shonanking@taku-MacBook-Air.local>
2017-12-08 00:13:40 +0900

    define Exception with Pos

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,29 +1,36 @@
 import scala.util.parsing.combinator._
-import scala.util.parsing.input.CharSequenceReader
+import scala.util.parsing.input.{CharSequenceReader, Position}
 import Ast._
 
-case class MyData( val d: Char, val a: Char ) {}
+case class MyData( val pos: Pos,val d: Char, val a: Char ) {}
 
 object OpegParser{
+    case class ParseException(pos: Pos, msg: String) extends Exception(pos + msg)
+
     object ParserCore extends Parsers {
         type Elem = Char;
+        lazy val loc: Parser[Position] = Parser{reader => Success(reader.pos, reader)} 
         val isDigit = elem( "DIGIT", { case c => '0' <= c && c <= '9' } );
         val isAlpha = elem( "ALPHA", { case c => 'a' <= c && c <= 'z' } );
-        val parser: Parser[ MyData ] = isDigit ~ isAlpha ^^ { case d ~ a => MyData( d, a ) };
+        val parser: Parser[ MyData ] = (loc) ~ isDigit ~ isAlpha ^^ { case pos ~ d ~ a => MyData(Pos(pos.line, pos.column), d, a ) };
     }
 
     def main(args: Array[String]) = {
         val g = parse("5a")
         println(g)
+        val e = parse("5A")
+        println(e)
     }
     
     def parse(doc: String):MyData  = {
         ParserCore.parser(new CharSequenceReader( doc )) match {
             case ParserCore.Success(node, _) => node
-            case ParserCore.Failure(msg, _) => 
-                throw new Exception
-            case ParserCore.Error(msg, _) =>
-                throw new Exception     
+            case ParserCore.Failure(msg, rest) => 
+                val pos = rest.pos
+                throw new ParseException(Pos(pos.line, pos.column), msg)
+            case ParserCore.Error(msg, rest) =>
+                val pos = rest.pos
+                throw new ParseException(Pos(pos.line, pos.column), msg)    
         }
     }
 }
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,9 +1,10 @@
 import org.scalatest._
+import Ast._
 
 class TestParse extends FunSuite {
 
   test("5a") {
-    assert(OpegParser.parse("5a") == MyData('5','a'))
+    assert(OpegParser.parse("5a") == MyData(Pos(1,1),'5','a'))
   }
 
 }
