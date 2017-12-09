a8d459357d9284614cdb29e15ff32f81cc16c6bc
taku tada <shonanking@taku-MacBook-Air.local>
2017-12-09 17:40:28 +0900

    add test math

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -19,4 +19,8 @@ class TestParse extends FunSuite {
     assert(GpegParser.parse(new FileReader("src/test/resources/GPEG/test4.gpeg")).toString == "Grammar(Pos(1,1),'A,Map('A -> CharClass(Pos(1,6),List(CharRange(a,z)))))")
   }
 
+  test("math") {
+    assert(GpegParser.parse(new FileReader("src/test/resources/GPEG/math.gpeg")).toString == "Grammar(Pos(1,1),'expression,Map('expression -> AnyNonterminal(Pos(1,15),'additive), 'additive -> Seq(Pos(2,23),AnyNonterminal(Pos(2,13),'multitive),Rep(Pos(2,23),Choice(Pos(2,44),Seq(Pos(2,28),Str(Pos(2,24),+),AnyNonterminal(Pos(2,28),'multitive)),Seq(Pos(2,44),Str(Pos(2,40),-),AnyNonterminal(Pos(2,44),'multitive))))), 'multitive -> Seq(Pos(3,20),AnyNonterminal(Pos(3,14),'value),Rep(Pos(3,20),Choice(Pos(3,37),Seq(Pos(3,25),Str(Pos(3,21),*),AnyNonterminal(Pos(3,25),'value)),Seq(Pos(3,37),Str(Pos(3,33),/),AnyNonterminal(Pos(3,37),'value))))), 'value -> Choice(Pos(4,34),Seq(Pos(4,10),CharClass(Pos(4,10),List(CharRange(0,9))),Rep(Pos(4,10),CharClass(Pos(4,10),List(CharRange(0,9))))),Seq(Pos(4,34),Seq(Pos(4,23),Str(Pos(4,19),(),AnyNonterminal(Pos(4,23),'expression)),Str(Pos(4,34),))))))")
+  }
+
 }
