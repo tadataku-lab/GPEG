cb5376e506e1fa9ec12d23d555d04d5e4251b697
taku tada <shonanking@taku-MacBook-Air.local>
2017-12-12 15:28:59 +0900

    redifine  AST

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -1,24 +1,43 @@
+ 
 object Ast {
+
+  case class Grammar(start: Exp, rules: Map[Symbol,Exp]) 
+  sealed trait Exp 
+  case class Empty() extends Exp
+  case class Any() extends Exp
+  case class AnyChar(body: Char) extends Exp
+  case class NonTerm(name: Symbol) extends Exp 
+  case class Seq(lhs: Exp, rhs: Exp) extends Exp
+  case class Choice(lhs: Exp, rhs: Exp) extends Exp
+  case class Alt(lhs: Exp, rhs: Exp) extends Exp 
+  case class Many(body: Exp) extends Exp 
+  case class Not(body: Exp) extends Exp 
+  case class And(body: Exp) extends Exp
+  case class Caputure(label: Symbol, body: Exp) extends Exp
+  case class FoldMany(label: Symbol, lhs: Exp, rhs: Exp) extends Exp 
+  case class LinkTree(label: Symbol, body: Exp) extends Exp
+
+  /*
   trait HasPosition { def pos: Pos }
   case class Pos(line: Int, column: Int)
-  case class Grammar(pos: Pos, start: Symbol, rules: Map[Symbol,Exp]) extends HasPosition // G
-  //case class Rule(pos: Pos, name: Symbol, body: Exp) extends HasPosition // R
+  case class Grammar(pos: Pos, start: Symbol, rules: Map[Symbol,Exp]) extends HasPosition 
   sealed trait Exp extends HasPosition
-  case class Empty(pos: Pos, emp: Unit) extends Exp // 'ε'
-  case class CharClass(pos: Pos, elems: List[CharClassElement]) extends Exp // 'a,b,c...'
-  case class Wildcard(pos: Pos) extends Exp // '.' only gpeg ?
-  case class AnyNonterminal(pos: Pos, name: Symbol) extends Exp // 'A,B,C...'
-  case class Seq(pos: Pos, lhs: Exp, rhs: Exp) extends Exp // 'e e'
-  case class Choice(pos: Pos, lhs: Exp, rhs: Exp) extends Exp // 'e/e'
-  case class Alt(pos: Pos, lhs: Exp, rhs: Exp) extends Exp // 'e|e' only gpeg
-  case class Rep(pos: Pos, body: Exp) extends Exp // 'e*'
-  case class Not(pos: Pos, body: Exp) extends Exp // '!e'
+  case class Empty(pos: Pos, emp: Unit) extends Exp
+  case class CharClass(pos: Pos, elems: List[CharClassElement]) extends Exp
+  case class Wildcard(pos: Pos) extends Exp 
+  case class AnyNonterminal(pos: Pos, name: Symbol) extends Exp 
+  case class Seq(pos: Pos, lhs: Exp, rhs: Exp) extends Exp
+  case class Choice(pos: Pos, lhs: Exp, rhs: Exp) extends Exp
+  case class Alt(pos: Pos, lhs: Exp, rhs: Exp) extends Exp 
+  case class Rep(pos: Pos, body: Exp) extends Exp 
+  case class Not(pos: Pos, body: Exp) extends Exp 
   case class And(pos: Pos, body: Exp) extends Exp
-  case class Caputure(pos: Pos, label: Symbol, body: Exp) extends Exp // 'L{e}' only cpeg 
-  case class FoldMany(pos: Pos, label: Symbol, lhs: Exp, rhs: Exp) extends Exp //e |L{e} only cpeg
+  case class Caputure(pos: Pos, label: Symbol, body: Exp) extends Exp
+  case class FoldMany(pos: Pos, label: Symbol, lhs: Exp, rhs: Exp) extends Exp 
   case class LinkTree(pos: Pos, label: Symbol, body: Exp) extends Exp
   case class Str(pos: Pos, target: String) extends Exp
   sealed trait CharClassElement
   case class OneChar(ch: Char) extends CharClassElement
-  case class CharRange(from: Char, to: Char) extends CharClassElement
+  case class CharRange(from: Char, to: Char) extends CharClassElement 
+  */
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,63 +5,65 @@ import Ast._
 import PegParser._
 
 object GpegParser{
+    case class Pos(line: Int, column: Int)
     case class ParseException(pos: Pos, msg: String) extends Exception(pos + msg)
 
     object ParserCore extends Parsers {
         type Elem = Char;
+        
         private val any: Parser[Char] = elem(".", c => c != CharSequenceReader.EofCh)
         private def chr(c: Char): Parser[Char] = c
         private def crange(f: Char, t: Char): Parser[Char] = elem("[]", c => f <= c && c <= t)
-        lazy val GRAMMER: Parser[Grammar] = (loc <~ Spacing) ~ Definition.+ <~ EndOfFile ^^ {
-            case pos ~ rules => Grammar(Pos(pos.line, pos.column), rules.head._1, rules.foldLeft(Map[Symbol,Exp]()){(m,r) => m.updated(r._1, r._2)})
+        lazy val GRAMMER: Parser[(Grammar, Pos)] = (loc <~ Spacing) ~ Definition.+ <~ EndOfFile ^^ {
+            case pos ~ rules => ( Grammar( rules.head._2, rules.foldLeft(Map[Symbol,Exp]()){(m,r) => m.updated(r._1, r._2)}), Pos(pos.line, pos.column))
         }
         lazy val Definition: Parser[(Symbol,Exp)] = (Nonterminal <~ (LEFTARROW | EQ)) ~ Expression <~ SEMI_COLON ^^ {
-            case n ~ b => (n.name, b)
+            case n ~ b => (n._1.name, b._1)
         }
-        lazy val Expression: Parser[Exp] = (
-            Sequence ~ (BAR ~> Sequence).+ ^^ { case x ~ xs => xs.foldLeft(x){(a, y) => Alt(y.pos, a, y)}}
-            | Sequence ~ (SLASH ~> Sequence).+ ^^ { case x ~ xs => xs.foldLeft(x){(a, y) => Choice(y.pos, a, y)}}
+        lazy val Expression: Parser[(Exp, Pos)] = (
+            Sequence ~ (BAR ~> Sequence).+ ^^ { case x ~ xs => xs.foldLeft(x){(a, y) => (Alt(a._1, y._1), y._2)}}
+            | Sequence ~ (SLASH ~> Sequence).+ ^^ { case x ~ xs => xs.foldLeft(x){(a, y) => (Choice(a._1, y._1), y._2)}}
             | Sequence
         )
-        lazy val Sequence: Parser[Exp] = Prefix.+ ^^ { case x::xs => 
-            xs.foldLeft(x){(a, y) => Seq(y.pos, a, y)}
+        lazy val Sequence: Parser[(Exp, Pos)] = Prefix.+ ^^ { case x::xs => 
+            xs.foldLeft(x){(a, y) => (Seq(a._1, y._1), y._2)}
         }
-        lazy val Prefix: Parser[Exp] = (
-            (loc <~ AND) ~ Suffix ^^ { case pos ~ e =>   Not(Pos(pos.line, pos.column),Not(Pos(pos.line, pos.column), e)) }
-            | (loc <~ NOT) ~ Suffix ^^ { case pos ~ e => Not(Pos(pos.line, pos.column), e) }
+        lazy val Prefix: Parser[(Exp, Pos)] = (
+            (loc <~ AND) ~ Suffix ^^ { case pos ~ e =>  ( And(e._1), Pos(pos.line, pos.column)) }
+            | (loc <~ NOT) ~ Suffix ^^ { case pos ~ e => ( Not(e._1), Pos(pos.line, pos.column)) }
             | Suffix
         )
-        lazy val Suffix: Parser[Exp] = (
-            loc ~ Primary <~ QUESTION ^^ { case pos ~ e => Choice(Pos(pos.line, pos.column), e, Empty(Pos(pos.line, pos.column), Unit)) }
-            | loc ~ Primary <~ STAR ^^ { case pos ~ e => Rep(Pos(pos.line, pos.column), e) }
-            | loc ~ Primary <~ PLUS ^^ { case pos ~ e => Seq(Pos(pos.line, pos.column), e, Rep(Pos(pos.line, pos.column), e)) }
+        lazy val Suffix: Parser[(Exp, Pos)] = (
+            loc ~ Primary <~ QUESTION ^^ { case pos ~ e => (Choice(e._1, Empty()), Pos(pos.line, pos.column)) }
+            | loc ~ Primary <~ STAR ^^ { case pos ~ e => (Many(e._1), Pos(pos.line, pos.column)) }
+            | loc ~ Primary <~ PLUS ^^ { case pos ~ e => (Seq(e._1, Many(e._1)), Pos(pos.line, pos.column)) }
             | Primary
         )
-        lazy val Primary: Parser[Exp] = (
+        lazy val Primary: Parser[(Exp, Pos)] = (
             Nonterminal 
             | OPEN ~> Expression <~ CLOSE
             | Literal
             | CLASS
-            | loc <~ DOT ^^ { case pos => Wildcard(Pos(pos.line, pos.column)) }
+            | loc <~ DOT ^^ { case pos => (Any(), Pos(pos.line, pos.column)) }
         )
-        lazy val Literal: Parser[Str] = loc ~ (
+        lazy val Literal: Parser[(Exp, Pos)] = loc ~ (
             chr('\'') ~> (not('\'') ~> CHAR).* <~ chr('\'') <~ Spacing
             | chr('"') ~> (not('"') ~> CHAR).* <~ chr('"') <~ Spacing
             ) ^^ {
-            case pos ~ cs => Str(Pos(pos.line, pos.column), cs.foldLeft(""){(acc, n) => acc + n})
+            case pos ~ (c::cs) => (cs.foldLeft(AnyChar(c): Exp){(a, y) => Seq(a, AnyChar(y))}, Pos(pos.line, pos.column))
         }
-        lazy val CLASS: Parser[CharClass] = (loc <~ chr('[')) ~ (not(chr(']')) ~> Range).* <~ ']' ~> Spacing ^^ {
-            case (pos ~ rs) => CharClass(Pos(pos.line, pos.column), rs);
+        lazy val CLASS: Parser[(Exp, Pos)] = (loc <~ chr('[')) ~ (not(chr(']')) ~> Range).* <~ ']' ~> Spacing ^^ {
+            case pos ~ (r::rs) => (rs.foldLeft(r){(a, y) => Seq(a, y)}, Pos(pos.line, pos.column));
         }
-        lazy val Range: Parser[CharClassElement] = (
-            CHAR ~ '-' ~ CHAR ^^ { case f~_~t => CharRange(f, t) }
-            | CHAR ^^ { case c => OneChar(c) }
+        lazy val Range: Parser[Exp] = (
+            CHAR ~ '-' ~ CHAR ^^ { case f~_~t => (f to t).foldRight(AnyChar(t): Exp){(x, acc) => Seq(AnyChar(x), acc)} }
+            | CHAR ^^ { case c => AnyChar(c) }
         )
         lazy val CHAR: Parser[Char] = ( 
             not('\\') ~ any ^^ { case _ ~ c => c}
         )
-        lazy val Nonterminal: Parser[AnyNonterminal] = loc ~ NonterminalStart ~ NonterminalCont.* <~ Spacing ^^ {
-            case pos ~ s ~ c => AnyNonterminal(Pos(pos.line, pos.column), Symbol("" + s + c.foldLeft("")(_ + _)))
+        lazy val Nonterminal: Parser[(NonTerm, Pos)] = loc ~ NonterminalStart ~ NonterminalCont.* <~ Spacing ^^ {
+            case pos ~ s ~ c => ( NonTerm(Symbol("" + s + c.foldLeft("")(_ + _))), Pos(pos.line, pos.column))
         }
         lazy val NonterminalStart: Parser[Char] = crange('a','z') | crange('A','Z') | '_'
         lazy val NonterminalCont: Parser[Char] = NonterminalStart | crange('0','9')
@@ -96,7 +98,7 @@ object GpegParser{
     
     def parse(content: java.io.Reader):Grammar  = {
         ParserCore.GRAMMER(StreamReader(content)) match {
-            case ParserCore.Success(node, _) => node
+            case ParserCore.Success(node, _) => node._1
             case ParserCore.Failure(msg, rest) => 
                 val pos = rest.pos
                 throw new ParseException(Pos(pos.line, pos.column), msg)
